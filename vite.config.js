240bbe260e1b4c0fa77ad41ec5500b4e1fe9d4c3
Pratyusha Chowdary <135346795+pratyusha-ravilla@users.noreply.github.com>
2026-04-15 16:09:50 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '127.0.0.1', // your custom IP
+    port: 8000         // your custom port
+  }
+})
